e8d2f2fdd8b61a401e340ced7000029c850ced85
rammohan <rammohan8555@gmail.com>
2024-10-06 11:09:39 +0530

    terrafor

---
+++ Day3/main.tf
@@ -0,0 +1,39 @@
+terraform {
+    required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.31.0" # AWS provider version, not terraform version
+    }
+    }
+}
+
+
+
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+
+resource "aws_instance" "rammohan" {
+    instance_type = "t2.micro"
+    ami = "ami-0866a3c8686eaeeba"
+    subnet_id = "subnet-061fd8c1851b15c3c"
+}
+
+
+resource "aws_s3_bucket" "s3_bucket" {
+    bucket = "mohan-s3-demo-tf-state"
+}
+
+
+resource "aws_dynamodb_table" "terraform_lock"{
+    name = "terraform-lock"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+      name = "LockID"
+      type = "S"
+    }
+}
