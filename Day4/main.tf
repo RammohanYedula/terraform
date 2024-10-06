7b7d80867619503c3eda118d8d4fa196649cd851
rammohan <rammohan8555@gmail.com>
2024-10-06 14:57:25 +0530

    terrafor

---
+++ Day4/main.tf
@@ -0,0 +1,100 @@
+# Define the AWS provider configuration.
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired AWS region.
+}
+
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+resource "aws_key_pair" "example" {
+    key_name = "terraform-demo-mohan"  #replace with your desired key name
+    public_key = file("C:/Users/rammo/.ssh/id_rsa.pub") #Replace with the path to your pulic key file
+  
+}
+
+resource "aws_vpc" "myvpc" {
+    cidr_block = var.cidr
+  
+}
+
+resource "aws_subnet" "terraform_subnet" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.0.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+    vpc_id = aws_vpc.myvpc.id
+  
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+}
+
+resource "aws_security_group" "websg" {
+    name = "web"
+    vpc_id = aws_vpc.myvpc.id
+
+    ingress {
+        description = "HTTP from VPC"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "SSH"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+        Name = "Web-sg"
+    }
+}
+
+resource "aws_instance" "server" {
+    ami = "ami-0866a3c8686eaeeba"
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.example.key_name
+    vpc_security_group_ids = [ aws_security_group.websg.id ]
+    subnet_id = aws_subnet.terraform_subnet.id
+
+    connection {
+        type = "ssh"
+        user = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+        private_key = file("C:/Users/rammo/.ssh/id_rsa")
+        host = self.public_ip
+    }
+
+    # File provisioner to copy a file from local to the remote EC2 instance
+
+    provisioner "file" {
+        source = "C:/Users/rammo/OneDrive/Desktop/DevOps/terraformPractice/Day4/app.py"  # Replace with the path to your local file
+        destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance   
+    }
+    provisioner "remote-exec" {
+        inline = [ 
+        "echo 'Hello from the remote instance'",
+        "sudo apt update -y",  # Update package lists (for ubuntu)
+        "sudo apt-get install -y python3-pip",  # Example package installation
+        "cd /home/ubuntu",
+        "sudo pip3 install flask",
+        "sudo python3 app.py &",
+        ]
+    }
+}
