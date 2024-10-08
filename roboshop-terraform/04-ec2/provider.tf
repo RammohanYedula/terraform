dc4cb3ea0882f2e778496addcc3b074f69e0bc10
rammohan <rammohan8555@gmail.com>
2024-10-08 18:11:16 +0530

    terraform-roboshop

---
+++ roboshop-terraform/04-ec2/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.31.0" # AWS provider version, not terraform version
+    }
+  }
+   
+   backend "s3" {
+    bucket = "mohan-aws-remote-state-terraform"
+    key    = "ec2"
+    region = "us-east-1"
+    dynamodb_table = "mohan-aws-locking"
+    }
+
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
